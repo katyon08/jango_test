6a5cdb6234e5a9efc79eff1c9d4adf07a57c0d43
vrossokhovatskiy <vrossokhovatskiy@dataart.com>
2017-11-08 16:40:12 +0200

    gitignore fix

--- myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\vrossokhovatskiy\projects\python\blog_root\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
